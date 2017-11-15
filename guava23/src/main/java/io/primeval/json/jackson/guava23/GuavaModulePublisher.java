cf8de7e6387b33bfac6abb5e6cdf01cb93748455
Simon Chemouil <simon.chemouil@lambdacube.fr>
2017-11-15 08:41:32 +0100

    Issue #3: improve build

---
+++ guava23/src/main/java/io/primeval/json/jackson/guava23/GuavaModulePublisher.java
@@ -0,0 +1,11 @@
+package io.primeval.json.jackson.guava23;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+
+@Component(service = Module.class)
+public final class GuavaModulePublisher extends GuavaModule {
+    // Nothing, just publish GuavaModule
+}
